5ec7147917f2daf33b96d5221c4d9adb48e8f9fb
JulianaCBB1 <julianacbbuzanello@gmail.com>
2026-04-09 12:13:25 +0200

    ran format script (no code changes)

--- test/vitest/approvals.spec.ts
+++ test/vitest/approvals.spec.ts
@@ -20,10 +20,9 @@ describe('Gilded Rose Approval', () => {
   });
 
   it('should thirtyDays', () => {
-    const consoleOutput = execSync(
-      'ts-node test/golden-master-text-test.ts 30',
-      { encoding: 'utf-8' }
-    );
+    const consoleOutput = execSync('ts-node test/golden-master-text-test.ts 30', {
+      encoding: 'utf-8',
+    });
 
     expect(consoleOutput).toMatchSnapshot();
   });
