2324f97e32543882d01f0ad889f2560a6c03d6c0
JulianaCBB1 <julianacbbuzanello@gmail.com>
2026-04-09 13:21:51 +0200

    updated golden master text to use subclass constructors

--- test/golden-master-text-test.ts
+++ test/golden-master-text-test.ts
@@ -1,18 +1,18 @@
-import { Item, GildedRose } from '../app/gilded-rose';
+import { ManagedItem, AgedBrie, Sulfuras, Conjured, BackstagePasses } from '@/item-types';
+import { GildedRose } from '../app/gilded-rose';
 
 console.log('OMGHAI!');
 
 const items = [
-  new Item('+5 Dexterity Vest', 10, 20), //
-  new Item('Aged Brie', 2, 0), //
-  new Item('Elixir of the Mongoose', 5, 7), //
-  new Item('Sulfuras, Hand of Ragnaros', 0, 80), //
-  new Item('Sulfuras, Hand of Ragnaros', -1, 80),
-  new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-  new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
-  new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
-  // this conjured item does not work properly yet
-  new Item('Conjured Mana Cake', 3, 6),
+  new ManagedItem('+5 Dexterity Vest', 10, 20),
+  new AgedBrie('Aged Brie', 2, 0),
+  new ManagedItem('Elixir of the Mongoose', 5, 7),
+  new Sulfuras('Sulfuras, Hand of Ragnaros', 0, 80),
+  new Sulfuras('Sulfuras, Hand of Ragnaros', -1, 80),
+  new BackstagePasses('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+  new BackstagePasses('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+  new BackstagePasses('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+  new Conjured('Conjured Mana Cake', 3, 6),
 ];
 
 const gildedRose = new GildedRose(items);
